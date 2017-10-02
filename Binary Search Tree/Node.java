4db94d835dd9a16c4e8c74af6d7f38f5303247f2
Peter Beattie <peterbeattie19@gmail.com>
2017-10-02 21:25:46 +0100

    Add files via upload

---
+++ Binary Search Tree/Node.java
@@ -0,0 +1,18 @@
+class Node{
+	int data;
+	Node left;
+	Node right;
+	public Node(int data){
+		this.data = data;
+		left = null;
+		right = null;
+	}
+
+	public int getData(){
+		return this.data; 
+	}
+
+	public void setData(int n){
+		this.data = n;
+	}
+}
