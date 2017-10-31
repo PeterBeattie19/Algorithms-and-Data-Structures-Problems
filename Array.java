0548901eb0939bdaf53e97356451598afd7fecc1
Peter Beattie <peterbeattie19@gmail.com>
2017-10-31 10:44:21 +0000

    Update Array.java

--- Array.java
+++ Array.java
@@ -110,6 +110,10 @@ public class Array{
     else if(type.equals("quick")){
       quickSort(arr, 0, arr.length-1); 
     }
+    
+    else if(type.equals("merge")){
+      mergesort(arr); 
+    }
   }
 
 
@@ -191,7 +195,56 @@ public class Array{
   }
 
 
+  public void mergesort(int arr[]){
+    int temp[] = new int[arr.length];
+    mergesort(arr, temp, 0, arr.length-1);
+  }
 
+  public void mergesort(int arr[], int temp[], int leftStart, int rightStart){
+    if(leftStart >= rightStart){
+      return;
+    }
+
+    int middle = (leftStart + rightStart) / 2;
+
+    mergesort(arr, temp, leftStart, middle);
+    mergesort(arr, temp, middle+1, rightStart);
+
+    mergeHalves(arr, temp, leftStart, rightStart);
+  }
+
+  public void mergeHalves(int arr[], int temp[], int leftStart, int rightEnd){
+    int leftEnd = (rightEnd + leftStart) / 2;  //Middle point in array
+    int rightStart = leftEnd + 1; //mid point plus 1
+    int size = rightEnd - leftStart + 1;
+
+    int left = leftStart;
+    int right = rightStart;
+    int index = leftStart;
+
+    while(left <= leftEnd && right <= rightEnd){
+      if(arr[left] <= arr[right]){
+        temp[index] = arr[left];
+        left++;
+      }
+
+      else {
+        temp[index] =arr[right];
+        right++;
+      }
+
+      index++;
+    }
+
+    System.arraycopy(arr, left, temp, index, leftEnd - left + 1);
+    System.arraycopy(arr, right, temp, index, rightEnd - right + 1);
+    System.arraycopy(temp, leftStart, arr, leftStart, size);
+  }
+
+
+
+    System.out.println();
+  }
 
 
         //SWAPPING METHOD
