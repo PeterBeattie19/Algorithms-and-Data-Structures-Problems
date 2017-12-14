05e38a34383069bf3d1a56543a49fe569c13199b
Peter Beattie <peterbeattie19@gmail.com>
2017-12-14 23:16:33 +0000

    Add files via upload

---
+++ BitManipulation.java
@@ -0,0 +1,10 @@
+public class BitManipulation {
+	public static void main(String[] args) {
+		System.out.println(1234^12); //Adds the two numbers, if they are not equal. Output:1246
+
+		System.out.println(1234^1234); //Output: 0 , Numbers are the same
+
+		System.out.println(21 & 21); 
+
+	}
+}
