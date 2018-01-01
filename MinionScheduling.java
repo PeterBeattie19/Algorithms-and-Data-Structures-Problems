68a92e4346773cd64679bcb98c9cf2e9ae6a011a
Peter Beattie <peterbeattie19@gmail.com>
2018-01-01 22:48:59 +0000

    Add files via upload

---
+++ MinionScheduling.java
@@ -0,0 +1,44 @@
+import java.util.*; 
+import java.io.*; 
+
+public class MinionScheduling {
+	public static void main (String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+
+		String line[] = br.readLine().split(" "); 
+		int n = Integer.parseInt(br.readLine());
+		int arr[] = new int[line.length]; 
+		HashMap <Integer,Integer> map = new HashMap<>(); 
+		int freq[]; 
+		int largest = 0; 
+
+		for(int i = 0; i<line.length; i++){
+			arr[i] = Integer.parseInt(line[i]); 
+
+			if(arr[i] > largest)
+				largest = arr[i];
+
+			if(map.containsKey(arr[i])){
+				int prev = map.get(arr[i]);
+				prev++;
+				map.put(arr[i],prev);
+
+			}
+
+			else{
+				map.put(arr[i],1); 
+			}
+
+		}
+
+
+		for(int i = 1; i<=largest; i++){
+			if(map.containsKey(i)){
+				if(map.get(i) >= n)
+					System.out.print(i +" "); 
+			}
+		}
+
+
+	}
+} 
